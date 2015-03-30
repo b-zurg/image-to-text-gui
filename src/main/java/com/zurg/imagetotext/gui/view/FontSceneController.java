244bbe41631f6dd7438875c30d3104dc1b9da3d1
Buzurg Arjmandi <zeebenzene@gmail.com>
2015-03-30 02:57:39 -0500

    Basic Font Scene done.

--- src/main/java/com/zurg/imagetotext/gui/view/FontSceneController.java
+++ src/main/java/com/zurg/imagetotext/gui/view/FontSceneController.java
@@ -1,13 +1,24 @@
 package com.zurg.imagetotext.gui.view;
 
+
+import java.awt.image.BufferedImage;
+import java.util.List;
+
+import recognition.FontInfo;
+import recognition.LetterOCR;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 
+
 import com.zurg.imagetotext.gui.Main;
+import com.zurg.imagetotext.model.LineViewData;
 
 public class FontSceneController {
 	private Main mainApp;
@@ -15,11 +26,44 @@ public class FontSceneController {
 	@FXML ImageView lineView;
 	@FXML TextField enterTextField;
 	@FXML Button goButton;
-	@FXML TableView fontTable;
-	@FXML TextArea finishedTextArea;
+	@FXML TableView<FontInfo> fontTable;
+	@FXML TableColumn<FontInfo, String> fontNameCol; 
+	@FXML TableColumn<FontInfo, String> distanceCol;
+	@FXML TableColumn<FontInfo, String> resultCol;
 	
-	public FontSceneController() {
+	
+	BufferedImage lineImage;
+	LetterOCR locr;
+	
+	@FXML
+	private void initialize() {		
+		locr = new LetterOCR();
+		setLineImage();
+		setColSettings();
+	}
+	
+	private void setLineImage() {
+		List<BufferedImage> lineImages = LineViewData.getParagraphAnalyzer().getLineSubImages();
+		lineImage = lineImages.get(1);
+		lineView.setImage(SwingFXUtils.toFXImage(lineImage, null));
+	}
+	
+	private void setColSettings() {
+		fontNameCol.setCellValueFactory(cell -> cell.getValue().getProcessedFontNameProperty());
+		distanceCol.setCellValueFactory(cell -> cell.getValue().getScoreProperty().asString());
+		resultCol.setCellValueFactory(cell -> cell.getValue().getOcrResultsProperty());
+	}
+	
+	@FXML
+	private void handleGoButton() {
+		String enteredText = enterTextField.getText();
+		System.out.println("entered text: " + enteredText);
+		List<FontInfo> fontInfo = locr.guessFonts(lineImage, enteredText);
 		
+		ObservableList<FontInfo> fonts = FXCollections.observableArrayList();
+		fonts.addAll(fontInfo);
+		fontTable.setItems(fonts);
+
 	}
 	
 	
